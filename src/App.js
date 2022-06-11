c9624ae851a90c589d7320b4c7e5d7b31cc67cb4
abdulbasith7145 <abdulbasith.vk@gmail.com>
2022-06-12 02:35:19 +0530

    commit -1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
       <h1>Hola {emoji.getUnicode("wave")}</h1>
       <h4>Glad to see you here! {emoji.getUnicode("heart_eyes")} {emoji.getUnicode("heart_eyes")}</h4>
-      <h6>I'll be back soon.!!</h6>
+      <h6>We'll be ruling here soon.!!</h6>
       </header>
     </div>
   );
