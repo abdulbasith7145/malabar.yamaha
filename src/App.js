4c4d082dd078b1d9da87a439a6976d829b748cde
abdulbasith7145 <abdulbasith.vk@gmail.com>
2022-06-12 01:54:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+const emoji = require("emoji-dictionary");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Hola {emoji.getUnicode("wave")}</h1>
+      <h4>Glad to see you here! {emoji.getUnicode("heart_eyes")} {emoji.getUnicode("heart_eyes")}</h4>
+      <h6>I'll be back soon.!!</h6>
       </header>
     </div>
   );
